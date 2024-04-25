9701439c572d40040cb7026b807dfb3a3b9c2bab
aaron <aaronblac@gmail.com>
2024-04-24 22:35:53 -0700

    variablize template

--- Src/logic.js
+++ Src/logic.js
@@ -1,8 +1,28 @@
 import data from "./data.js";
 
 $(function() {
+
+
+ 
+ const itemCardTemplate = ` {{ #itemCard }}
+            {{#if (@index < getNumberOfItemCards())}} 
+                <div class="card p-2 gap-1 round flex flex-column align-content-center ">
+                    <div class="container--item-thumb">
+                        <img src="{{ image }}" alt="{{ name }} Image">
+                    </div>
+                    <div class=" text-center">
+                        <span>{{ name }}</span>
+                    </div>
+                    <div class="flex justify-content-center">
+                        <a href="Pages/view-iteåçm.html">
+                            <button role="button" class="button-primary button-small">View Item</button>
+                        </a>
+                    </div>
+                </div>
+            {{/if}}
+            {{ /itemCard }}`
+
     // itemCard
-    const itemCardTemplate = $("#itemCardTemplate").html();
     Mustache.parse(itemCardTemplate);
     const itemCardRendered = Mustache.render(itemCardTemplate, { itemCard: data.products,currentIndex: (currentCard, index) => index });
     $("#FeaturedItemsHome").html(itemCardRendered);
