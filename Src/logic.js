11761622b545cf971de975335eaa88f219908b49
aaron <aaronblac@gmail.com>
2024-04-24 22:42:09 -0700

    move function

--- Src/logic.js
+++ Src/logic.js
@@ -1,29 +1,8 @@
 import data from "./data.js";
 
 $(function() {
-
-
- 
- const itemCardTemplate = ` 
-    {{ #itemCard }}
-    {{#if (@index < getNumberOfItemCards())}} 
-        <div class="card p-2 gap-1 round flex flex-column align-content-center ">
-            <div class="container--item-thumb">
-                <img src="{{ image }}" alt="{{ name }} Image">
-            </div>
-            <div class=" text-center">
-                <span>{{ name }}</span>
-            </div>
-            <div class="flex justify-content-center">
-                <a href="Pages/view-iteåçm.html">
-                    <button role="button" class="button-primary button-small">View Item</button>
-                </a>
-            </div>
-        </div>
-    {{/if}}
-    {{ /itemCard }}`;
-
     // itemCard
+    const itemCardTemplate = $("#itemCardTemplate").html();
     Mustache.parse(itemCardTemplate);
     const itemCardRendered = Mustache.render(itemCardTemplate, { itemCard: data.products,currentIndex: (currentCard, index) => index });
     $("#FeaturedItemsHome").html(itemCardRendered);
@@ -45,14 +24,14 @@ $(function() {
     Mustache.parse(typeCheckboxTemplate);
     const typeCheckboxRendered = Mustache.render(typeCheckboxTemplate, { type: data.type });
     $("#TypeCheckboxes").html(typeCheckboxRendered);
+    
+    const getNumberOfItemCards = () => {
+        if (window.innerWidth >= 992) { 
+            return 4;
+          } 
+    }
+    
+    
+    const numberOfCards = getNumberOfItemCards();
+    console.log(numberOfCards);
 });
-
-const getNumberOfItemCards = () => {
-    if (window.innerWidth >= 992) { 
-        return 4;
-      } 
-}
-
-
-const numberOfCards = getNumberOfItemCards();
-console.log(numberOfCards);
