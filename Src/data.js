e6c07f64a3976b393b9d8f09bfed3412ce69b199
aaron <aaronblac@gmail.com>
2024-04-24 21:18:42 -0700

    add mustache, jquery

---
+++ Src/data.js
@@ -0,0 +1,52 @@
+
+const products = [
+    {
+        name: "Boom Horse T-shirt",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Boom Horse Hat",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Boom Horse Logo Sticker",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Boom Horse Bag",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Boom Horse Long Sleeve",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Boom Horse Mug",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Boom Horse Tank Top",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Boom Horse Skateboard",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+];
+
+const sizes = ["Small", "Medium", "Large", "X-large", "XX-Large"];
+
+const gender = ["Men's", "Women's", "Unisex"];
+
+const type = ["Long Sleeve", "T-shirts", "Tank Tops", "Hats", "Skateboards", "Stickers", "Bags", "Mugs"];
+
+
+export default {products, sizes, gender, type};
---
+++ Src/logic.js
@@ -0,0 +1,27 @@
+import data from "./data";
+
+$(function() {
+    // itemCard
+    const itemCardTemplate = $("#itemCardTemplate").html();
+    Mustache.parse(itemCardTemplate);
+    const itemCardRendered = Mustache.render(itemCardTemplate, { itemCard: data.products });
+    $("#FeaturedItemsHome").html(itemCardRendered);
+
+    // sizes
+    const sizeCheckboxTemplate = $("#sizeCheckboxTemplate").html();
+    Mustache.parse(sizeCheckboxTemplate);
+    const sizeCheckboxRendered = Mustache.render(sizeCheckboxTemplate, { size: data.sizes });
+    $("#SizeCheckboxes").html(sizeCheckboxRendered);
+
+    // gender
+    const genderTemplate = $("#genderCheckboxTemplate").html();
+    Mustache.parse(genderTemplate);
+    const genderRendered = Mustache.render(genderTemplate, { gender: data.gender });
+    $("#GenderCheckboxes").html(genderRendered);
+
+    // type
+    const typeCheckboxTemplate = $("#typeCheckboxTemplate").html();
+    Mustache.parse(typeCheckboxTemplate);
+    const typeCheckboxRendered = Mustache.render(typeCheckboxTemplate, { type: data.type });
+    $("#TypeCheckboxes").html(typeCheckboxRendered);
+});
