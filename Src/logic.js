d76d6af19632a75a5263c650824e7375b572a1ed
aaron <aaronblac@gmail.com>
2024-04-27 17:58:03 -0700

    update impoort

--- Src/logic.js
+++ Src/logic.js
@@ -1,28 +1,28 @@
-import data from "./data.js";
+import { products, size, gender, type } from "./data.js";
 
 $(function() {
     // itemCard
     const itemCardTemplate = $("#itemCardTemplate").html();
     Mustache.parse(itemCardTemplate);
-    const itemCardRendered = Mustache.render(itemCardTemplate, { itemCard: data.products });
+    const itemCardRendered = Mustache.render(itemCardTemplate, { itemCard: products });
     $("#FeaturedItems").html(itemCardRendered);
 
     // sizes
     const sizeCheckboxTemplate = $("#sizeCheckboxTemplate").html();
     Mustache.parse(sizeCheckboxTemplate);
-    const sizeCheckboxRendered = Mustache.render(sizeCheckboxTemplate, { size: data.size, products: data.products });
+    const sizeCheckboxRendered = Mustache.render(sizeCheckboxTemplate, { size: size, products: products });
     $("#SizeCheckboxes").html(sizeCheckboxRendered);
 
     // gender
     const genderTemplate = $("#genderCheckboxTemplate").html();
     Mustache.parse(genderTemplate);
-    const genderRendered = Mustache.render(genderTemplate, { gender: data.gender, products: data.products });
+    const genderRendered = Mustache.render(genderTemplate, { gender: gender, products: products });
     $("#GenderCheckboxes").html(genderRendered);
 
     // type
     const typeCheckboxTemplate = $("#typeCheckboxTemplate").html();
     Mustache.parse(typeCheckboxTemplate);
-    const typeCheckboxRendered = Mustache.render(typeCheckboxTemplate, { type: data.type, products: data.products });
+    const typeCheckboxRendered = Mustache.render(typeCheckboxTemplate, { type: type, products: products });
     $("#TypeCheckboxes").html(typeCheckboxRendered);
     
     $(document).on('change','input[type=checkbox', () => {
